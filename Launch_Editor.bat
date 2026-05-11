33d0d3acc5f8209bfc97cd499462981312165b65
soroeru-afk <soroeru@gmail.com>
2026-05-11 11:39:47 +0900

    Update CHAT formatting with invisible markers (v15)

---
+++ Launch_Editor.bat
@@ -0,0 +1,3 @@
+@echo off
+start "" pythonw.exe "c:\Users\soroe\Documents\A-App\SOLID-SQUARE-EDITOR\launcher.py"
+exit
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "solid-square-editor-cache-v14";
+const CACHE_NAME = "solid-square-editor-cache-v15";
 const ASSETS = [
   "./",
   "./index.html",
